70cf70a247627623bd12d66e4529a5033b672307
Arttu Manninen <arttu@kaktus.cc>
2022-03-17 06:16:45 +0200

    Allow passing application root as an option, use relative imports

--- server/application.js
+++ server/application.js
@@ -3,14 +3,18 @@ const EventEmitter = require('events')
 const Logger = require('@adrenalin/logger')
 const helpers = require('@adrenalin/helpers.js')
 
+const getValue = helpers.getValue
+
 module.exports = async (opts = {}) => {
+  const options = opts || {}
+
   const logger = new Logger('Application')
   logger.setLevel(5)
   logger.info('Start initializing application')
 
   const app = require('express')()
   app.disable('x-powered-by')
-  app.APPLICATION_ROOT = path.join(__dirname, '..')
+  app.APPLICATION_ROOT = getValue(options, 'applicationRoot', path.join(__dirname, '..'))
   app.environment = process.env.ENVIRONMENT || 'public'
   app.helpers = helpers
 
@@ -46,56 +50,56 @@ module.exports = async (opts = {}) => {
 
   logger.debug('Get configuration')
   logger.ts = Date.now()
-  await require(path.join(app.APPLICATION_ROOT, 'server/configuration'))(app, opts)
+  await require('./configuration')(app, opts)
   logger.dt('Configuration loaded')
   app.config.set(opts || {})
 
   // logger.debug('Apply constants')
-  // await require(path.join(app.APPLICATION_ROOT, 'server/constants'))(app)
+  // await require('./constants')(app, opts)
   // logger.dt('Applied constants')
   //
   // logger.debug('Apply services')
-  // await require(path.join(app.APPLICATION_ROOT, 'server/services'))(app)
+  // await require('./services')(app, opts)
   // logger.dt('Applied services')
   //
   // logger.debug('Apply error handlers')
-  // await require(path.join(app.APPLICATION_ROOT, 'server/errors'))(app)
+  // await require('./errors')(app, opts)
   // logger.dt('Applied error handlers')
   //
   logger.debug('Apply renderers')
-  await require(path.join(app.APPLICATION_ROOT, 'server/renderers'))(app)
+  await require('./renderers')(app, opts)
   logger.dt('Applied renderers')
 
   // logger.debug('Apply body parsers')
-  // await require(path.join(app.APPLICATION_ROOT, 'server/bodyparsers'))(app)
+  // await require('./bodyparsers')(app, opts)
   // logger.dt('Applied body parsers')
   //
   // logger.debug('Apply Redis connection')
-  // await require(path.join(app.APPLICATION_ROOT, 'server/redis'))(app)
+  // await require('./redis')(app, opts)
   // logger.dt('Applied Redis connection')
   //
   // logger.debug('Apply cache')
-  // await require(path.join(app.APPLICATION_ROOT, 'server/cache'))(app)
+  // await require('./cache')(app, opts)
   // logger.dt('Applied cache')
   //
   // logger.debug('Apply static paths')
-  // await require(path.join(app.APPLICATION_ROOT, 'server/static'))(app)
+  // await require('./static')(app, opts)
   // logger.dt('Applied static paths')
   //
   // logger.debug('Apply session storage')
-  // await require(path.join(app.APPLICATION_ROOT, 'server/session'))(app)
+  // await require('./session')(app, opts)
   // logger.dt('Applied session storage')
   //
   // logger.debug('Apply logger')
-  // await require(path.join(app.APPLICATION_ROOT, 'server/log'))(app)
+  // await require('./logger')(app, opts)
   // logger.dt('Applied logger')
   //
   // logger.debug('Apply locales')
-  // await require(path.join(app.APPLICATION_ROOT, 'server/locales'))(app)
+  // await require('./locales')(app, opts)
   // logger.dt('Applied locales')
   //
   logger.debug('Apply routes')
-  await require(path.join(app.APPLICATION_ROOT, 'server/routes'))(app)
+  await require('./routes')(app, opts)
   logger.dt('Applied routes')
 
   return app
