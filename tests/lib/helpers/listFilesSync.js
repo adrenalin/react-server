7eb33a48b7ef046e5816570961186c264b73cc48
Arttu Manninen <arttu@kaktus.cc>
2022-03-17 05:45:00 +0200

    listFilesSync test unit tests

---
+++ tests/lib/helpers/listFilesSync.js
@@ -0,0 +1,41 @@
+const path = require('path')
+const expect = require('expect.js')
+const listFilesSync = require('../../../lib/helpers/listFilesSync')
+
+describe('lib/helpers/listFilesSync', () => {
+  const resourceDir = path.join(__dirname, '..', '..', 'resources', 'lib', 'helpers', 'listFilesSync')
+
+  it('should list files recursively', (done) => {
+    const files = listFilesSync(resourceDir)
+    expect(files).to.contain(path.join(resourceDir, 'test.txt'))
+    expect(files).to.contain(path.join(resourceDir, 'test.yml'))
+    expect(files).to.contain(path.join(resourceDir, 'test-dir', 'test-subdir', 'test.xml'))
+    done()
+  })
+
+  it('should apply extension filter', (done) => {
+    const files = listFilesSync(resourceDir, { ext: 'txt' })
+    expect(files).to.contain(path.join(resourceDir, 'test.txt'))
+    expect(files).not.to.contain(path.join(resourceDir, 'test.yml'))
+    expect(files).not.to.contain(path.join(resourceDir, 'test-dir', 'test-subdir', 'test.xml'))
+    done()
+  })
+
+  it('should apply extension filter as an array', (done) => {
+    const files = listFilesSync(resourceDir, { ext: ['txt', 'yml'] })
+    expect(files).to.contain(path.join(resourceDir, 'test.txt'))
+    expect(files).to.contain(path.join(resourceDir, 'test.yml'))
+    expect(files).not.to.contain(path.join(resourceDir, 'test-dir', 'test-subdir', 'test.xml'))
+    done()
+  })
+
+  it('should throw an error for an invalid path', (done) => {
+    expect(listFilesSync).withArgs(path.join(resourceDir, 'foobar')).to.throwError()
+    done()
+  })
+
+  it('should accept null options', (done) => {
+    listFilesSync(resourceDir, null)
+    done()
+  })
+})
