b14ae1cd84663d4fa60ab8a1524c8b8b25fd0e8b
Arttu Manninen <arttu@kaktus.cc>
2022-03-21 21:27:10 +0200

    Cache service

---
+++ services/cache.js
@@ -0,0 +1,63 @@
+const errors = require('@adrenalin/errors')
+const Service = require('./')
+const Cache = require('../lib/cache')
+
+class CacheService extends Service {
+  static get SERVICE_NAME () {
+    return 'cache'
+  }
+
+  /**
+   * Register database service
+   *
+   * @return { CacheService }         This instance
+   */
+  async register () {
+    this.cache = Cache.getEngine(this.app, this.config.get('engine', 'memcache'))
+    this.cache.setStorageKey(this.config.get('storageKey') || 'cache')
+
+    return this
+  }
+
+  /**
+   * Get value
+   *
+   * @param { string } key            Storage key
+   * @param { mixed } defaultValue    Default value
+   * @return { mixed }                Stored value
+   */
+  async get (key, defaultValue) {
+    return await this.cache.get(key, defaultValue)
+  }
+
+  /**
+   * Set value
+   *
+   * @param { string } key            Storage key
+   * @param { mixed } value           Storage value
+   * @param { number } expires        Number of seconds the value should be stored
+   * @return { Promise }              Resolves with self
+   */
+  async set (key, value, expires) {
+    return await this.cache.set(key, value, expires)
+  }
+
+  /**
+   * Delete a key
+   *
+   * @param { string } key            Storage key
+   */
+  async del (key) {
+    return await this.cache.del(key)
+  }
+
+  getClient () {
+    if (typeof this.cache.getClient !== 'function') {
+      throw new errors.NotImplemented(`Method "getEngine" is not defined in cache engine "${this.cache.constructor.name}"`)
+    }
+
+    return this.cache.getClient()
+  }
+}
+
+module.exports = CacheService
---
+++ services/index.js
@@ -0,0 +1,25 @@
+const Logger = require('@adrenalin/logger')
+
+class Service {
+  static get LOG_LEVEL () {
+    return 3
+  }
+
+  static get DEPENDENCIES () {
+    return null
+  }
+
+  constructor (app) {
+    this.app = app
+    this.helpers = app.helpers
+    this.logger = new Logger(this)
+
+    // Local configuration for the service
+    this.config = new app.helpers.Config()
+    this.config.set(app.config.get(this.constructor.SERVICE_NAME || this.constructor.name.toLowerCase()))
+
+    this.logger.setLevel(this.constructor.LOG_LEVEL)
+  }
+}
+
+module.exports = Service
