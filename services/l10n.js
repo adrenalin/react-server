0d83861c4481b1d2fc291e5294c623523150e0aa
Arttu Manninen <arttu@kaktus.cc>
2022-03-23 20:57:45 +0200

    Localization service

---
+++ services/l10n.js
@@ -0,0 +1,157 @@
+const fs = require('fs')
+const path = require('path')
+const YAML = require('yaml')
+const Service = require('./')
+const listFilesSync = require('../lib/helpers/listFilesSync')
+
+const registered = {}
+
+module.exports = class LocalizationService extends Service {
+  static get LOG_LEVEL () {
+    return 3
+  }
+
+  static get SERVICE_NAME () {
+    return 'l10n'
+  }
+
+  async register () {
+    const files = listFilesSync(this.getLocalesPath())
+
+    this.l10n = new this.helpers.Localization()
+    this.loadLocales(files)
+  }
+
+  /**
+   * Get locales path
+   *
+   * @return { string }               Path to the locales root
+   */
+  getLocalesPath () {
+    return this.app.config.get('services.l10n.path') || path.join(this.app.APPLICATION_ROOT, 'locales')
+  }
+
+  /**
+   * Get localization instance
+   *
+   * @return { Localization }         Localization instance
+   */
+  getInstance () {
+    return this.l10n
+  }
+
+  /**
+   * Get registered locales
+   *
+   * @param { string } [lang]             Language
+   * @param { boolean } [ignorePrivate]   Ignore private locales
+   * @return { object }                   Registered locales
+   */
+  getLocales (lang = null, ignorePrivate = true) {
+    const list = {}
+
+    for (const locale in registered) {
+      if (ignorePrivate && registered[locale].private) {
+        continue
+      }
+      list[locale] = this.helpers.copyObject(lang ? registered[locale].translations[lang] : registered[locale].translations)
+    }
+
+    return list
+  }
+
+  /**
+   * Get a localized string
+   *
+   * @param { string } lang           Language
+   * @param { mixed } ...args         Rest of the arguments
+   */
+  get (lang, ...args) {
+    return this.l10n.getInLang(lang, ...args)
+  }
+
+  /**
+   * Register locales
+   *
+   * @param { object } locales        Locales to register
+   * @return { LocalizationService }  This instance
+   */
+  registerLocales (locales) {
+    const aliases = {}
+
+    for (const locale in locales) {
+      const d = locales[locale]
+
+      const translations = d.translations || d || {}
+
+      registered[locale] = {
+        private: !!d.private,
+        context: d.context || null,
+        translations
+      }
+
+      if (d.alias) {
+        aliases[locale] = d.alias
+        continue
+      }
+
+      this.helpers.Localization.registerLocale(locale, translations)
+    }
+
+    for (const locale in aliases) {
+      const alias = aliases[locale]
+
+      if (!registered[alias]) {
+        this.logger.warn('Could not find the alias', alias, 'for', locale)
+        continue
+      }
+
+      const translations = registered[locale].translations = registered[alias].translations || {}
+      this.helpers.Localization.registerLocale(locale, translations)
+    }
+
+    return this
+  }
+
+  /**
+   * Load locales from a file
+   *
+   * @param { string|array } filename   Locale files to load
+   */
+  loadLocales (filename) {
+    this.helpers.castToArray(filename).forEach((fn) => {
+      try {
+        if (fn.match(/^[a-zA-Z0-9\-_]+$/)) {
+          this.logger.log(`Bare filename "${fn}" given, add application root and ".yml" suffix`)
+          fn = path.join(this.getLocalesPath(), `${fn}.yml`)
+          this.logger.debug(`Heuristics defined path is "${fn}"`)
+        }
+
+        if (!fn.match(/\.ya?ml$/)) {
+          this.logger.log('Skip', fn, 'because it does not have the suffix .yml or .yaml')
+          return
+        }
+
+        const content = fs.readFileSync(fn, 'utf-8')
+        const locales = YAML.parse(content)
+
+        for (const locale in locales) {
+          if (registered[locale]) {
+            this.logger.log(`Duplicate locale "${locale}" found in "${fn}"`)
+          }
+        }
+
+        this.registerLocales(locales)
+      } catch (err) {
+        /* istanbul ignore next error logging */
+        this.logger.error('Failed to load locales for', fn, 'due to', err.message)
+
+        /* istanbul ignore next error logging */
+        this.logger.error(err.stack)
+
+        /* istanbul ignore next rethrowing an error */
+        throw err
+      }
+    })
+  }
+}
---
+++ tests/services/l10n.js
@@ -0,0 +1,155 @@
+const path = require('path')
+const expect = require('expect.js')
+const { Localization } = require('@adrenalin/helpers.js')
+const init = require('../init')
+
+const Service = require('../../services')
+const L10nService = require('../../services/l10n')
+
+describe('services/l10n', () => {
+  let app, localesPath
+
+  before(async () => {
+    app = await init()
+    localesPath = app.config.get('services.l10n.path')
+  })
+
+  beforeEach(async () => {
+    app.config.set('services.l10n.path', localesPath)
+  })
+
+  it('should load l10n service', (done) => {
+    const l10n = new L10nService(app)
+    expect(l10n).to.be.a(Service)
+    done()
+  })
+
+  it('should automatically load the configured locales', async () => {
+    app.config.set('services.l10n.path', path.join(__dirname, '..', 'resources', 'services', 'l10n'))
+    const l10n = new L10nService(app)
+    await l10n.register()
+
+    expect(l10n.get('fi', 'autoload')).to.eql('Automaattisesti ladattu')
+    expect(l10n.get('en', 'autoload')).to.eql('Automatically loaded')
+  })
+
+  it('should return a Localization instance', async () => {
+    const l10n = new L10nService(app)
+    await l10n.register()
+
+    const instance = l10n.getInstance()
+    expect(instance).to.be.a(Localization)
+  })
+
+  it('should get public registered locales and ignore private', async () => {
+    const testLocales = {
+      testLocale: {
+        translations: {
+          fi: 'test-locale-fi',
+          en: 'test-locale-en'
+        }
+      },
+      testPrivateLocale: {
+        private: true,
+        translations: {
+          fi: 'test-private-locale-fi',
+          en: 'test-private-locale-en'
+        }
+      }
+    }
+    const l10n = new L10nService(app)
+    await l10n.register()
+    l10n.registerLocales(testLocales)
+
+    const locales = l10n.getLocales()
+    expect(locales).to.have.property('testLocale')
+    expect(locales.testLocale).to.eql(testLocales.testLocale.translations)
+
+    expect(locales).not.to.have.property('testPrivateLocale')
+  })
+
+  it('should get all registered locales when ignorePrivate flag is false', async () => {
+    const testLocales = {
+      testPrivateLocale: {
+        private: true,
+        translations: {
+          fi: 'test-private-locale-fi',
+          en: 'test-private-locale-en'
+        }
+      }
+    }
+    const l10n = new L10nService(app)
+    await l10n.register()
+    l10n.registerLocales(testLocales)
+
+    const locales = l10n.getLocales(null, false)
+    expect(locales).to.have.property('testPrivateLocale')
+  })
+
+  it('should get flat key-value object when language is defined', async () => {
+    const testLocales = {
+      testLocale: {
+        translations: {
+          fi: 'test-locale-fi',
+          en: 'test-locale-en'
+        }
+      }
+    }
+    const l10n = new L10nService(app)
+    await l10n.register()
+    l10n.registerLocales(testLocales)
+
+    const locales = l10n.getLocales('fi', false)
+    expect(locales).to.have.property('testLocale')
+    expect(locales.testLocale).to.eql(testLocales.testLocale.translations.fi)
+  })
+
+  it('should register locale aliases', async () => {
+    const testLocales = {
+      testLocale: {
+        translations: {
+          fi: 'test-locale-fi',
+          en: 'test-locale-en'
+        }
+      },
+      testAlias: {
+        alias: 'testLocale'
+      }
+    }
+    const l10n = new L10nService(app)
+    await l10n.register()
+    l10n.registerLocales(testLocales)
+
+    expect(l10n.get('fi', 'testAlias')).to.eql(l10n.get('fi', 'testLocale'))
+  })
+
+  it('should not register an alias to an undefined locale', async () => {
+    const l10n = new L10nService(app)
+    await l10n.register()
+    l10n.registerLocales({
+      undefinedAlias: {
+        alias: 'testServicesL10nAliasNotFound'
+      }
+    })
+
+    expect(l10n.get('fi', 'undefinedAlias')).to.eql('undefinedAlias')
+  })
+
+  it('should load a locale file with suffix', async () => {
+    const testFile = path.join(__dirname, '..', 'resources', 'services', 'l10n', 'load-with-suffix.yml')
+    const l10n = new L10nService(app)
+    await l10n.register()
+    l10n.loadLocales(testFile)
+    expect(l10n.get('en', 'loadWithSuffix')).to.eql('Load with suffix')
+  })
+
+  it('should add .yml suffix when omitted', async () => {
+    const testFile = 'load-without-suffix'
+
+    app.config.set('services.l10n.path', path.join(__dirname, '..', 'resources', 'services', 'l10n'))
+    const l10n = new L10nService(app)
+    await l10n.register()
+    l10n.loadLocales(testFile)
+    expect(l10n.get('en', 'loadWithoutSuffix')).to.eql('Load without suffix')
+  })
+})
