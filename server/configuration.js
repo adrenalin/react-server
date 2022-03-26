511a9329f5cc76e80fdab1bda02b2bd41c48f583
Arttu Manninen <arttu@kaktus.cc>
2022-03-26 11:37:28 +0200

    Set configuration schema

--- server/configuration.js
+++ server/configuration.js
@@ -8,12 +8,15 @@ module.exports = async (app, overrides) => {
   logger.debug('Load configuration')
 
   const config = app.config = new ServerConfig()
+  const schema = require(path.join(__dirname, '..', 'schemas', 'config.json'))
 
   config
     .loadFile(path.join(app.APPLICATION_ROOT, 'config', 'defaults.yml'))
     .loadFile(path.join(app.APPLICATION_ROOT, 'config', `${app.environment}.yml`), true)
     .loadFile(path.join(app.APPLICATION_ROOT, 'config', 'local.yml'), true)
     .loadFile(path.join(app.APPLICATION_ROOT, 'config', `local-${app.environment}.yml`), true)
+
+  config.setSchema(schema)
 
   logger.log('Configuration loaded')
   logger.debug('Loaded configuration', config.get())
