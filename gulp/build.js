41b827e7dde20660039dbf302f961adbbf19c791
Arttu Manninen <arttu@kaktus.cc>
2022-11-23 16:18:03 +0200

    Build gulp task

---
+++ gulp/build.js
@@ -0,0 +1,31 @@
+const gulp = require('gulp')
+const { getValue } = require('@adrenalin/helpers.js')
+const config = require('./config')
+
+// Load tasks
+const { compileStyles, concatVendors, copyVendors, copyFiles, copyFonts, copyImages, copyJS, bundler, lintSCSS } = require('./tasks')
+
+const tasks = [
+  concatVendors,
+  compileStyles,
+  copyFiles,
+  copyFonts,
+  copyImages,
+  copyJS,
+  bundler,
+  lintSCSS
+]
+
+if (config.get('build.client')) {
+  const buildClient = require('./tasks/buildClient')
+  tasks.push(buildClient)
+}
+
+if (config.get('apidoc.build')) {
+  const buildAPIdoc = require('./tasks/buildApiDoc')
+  tasks.push(buildAPIdoc)
+}
+
+module.exports = function build () {
+  return gulp.parallel(tasks)
+}
