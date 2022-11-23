a763b8246835ded16191311a57d37ccf985ac118
Arttu Manninen <arttu@kaktus.cc>
2022-11-23 16:17:31 +0200

    Use buildClient to install the package

--- install.js
+++ install.js
@@ -3,6 +3,11 @@ const path = require('path')
 const gulp = require('gulp')
 const babel = require('gulp-babel')
 
+const config = require('./gulp/config')
+config.set('root', __dirname)
+
+const buildClient = require('./gulp/tasks/buildClient')
+
 const babelrcRaw = fs.readFileSync(path.join(__dirname, '.babelrc'))
 const babelrc = JSON.parse(babelrcRaw)
 
@@ -15,16 +20,4 @@ if (reactstrap.includes('node_modules') && fs.existsSync(reactstrap) && !fs.exis
 }
 
 // Build client that uses ES6 + JSX
-if (__dirname.includes('node_modules') || process.env.REACT_SERVER_FORCE_BUILD === 'true') {
-  gulp
-    .src([
-      path.join(__dirname, 'client', '*.js'),
-      path.join(__dirname, 'client', '**', '*.js')
-    ])
-    .pipe(babel(babelrc))
-    .on('error', (err) => {
-      console.error('Caught an error', err)
-      process.exit(1)
-    })
-    .pipe(gulp.dest(path.join(__dirname, 'dist')))
-}
+buildClient()
