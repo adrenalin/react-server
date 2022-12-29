3d06a87f31f5f25ba98843493bd4267ffd945c61
Arttu Manninen <arttu@kaktus.cc>
2022-12-29 18:40:52 +0200

    Added support to override application root for config loader

--- server/config.js
+++ server/config.js
@@ -7,6 +7,13 @@ module.exports = (app, overrides) => {
   logger.setLevel(3)
   logger.debug('Load configuration')
 
+  const defaults = {
+    APPLICATION_ROOT: path.join(__dirname, '..')
+  }
+
+  app = app || defaults
+  const appRoot = overrides.APPLICATION_ROOT || app.APPLICATION_ROOT || defaults.APPLICATION_ROOT
+
   const config = app.config = new ServerConfig()
   const schema = require(path.join(__dirname, '..', 'schemas', 'config.json'))
 
@@ -18,10 +25,10 @@ module.exports = (app, overrides) => {
   config.setSchema(schema)
 
   config
-    .loadFile(path.join(app.APPLICATION_ROOT, 'config', 'defaults.yml'))
-    .loadFile(path.join(app.APPLICATION_ROOT, 'config', `${app.environment}.yml`), true)
-    .loadFile(path.join(app.APPLICATION_ROOT, 'config', 'local.yml'), true)
-    .loadFile(path.join(app.APPLICATION_ROOT, 'config', `local-${app.environment}.yml`), true)
+    .loadFile(path.join(appRoot, 'config', 'defaults.yml'))
+    .loadFile(path.join(appRoot, 'config', `${app.environment}.yml`), true)
+    .loadFile(path.join(appRoot, 'config', 'local.yml'), true)
+    .loadFile(path.join(appRoot, 'config', `local-${app.environment}.yml`), true)
 
   logger.log('Configuration loaded')
   logger.debug('Loaded configuration', config.get())
