115afa2567a31078f97fd0eccd5e4ff8b1d09f0f
Arttu Manninen <arttu@kaktus.cc>
2023-01-14 19:27:40 +0200

    Expose the new builder

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ module.exports = {
   Notifications: require('./dist/shared/notifications'),
 
   // Alt
+  buildStore: require('./dir/lib/builder'),
   BaseSourceBuilder: require('./dist/data/builder'),
   BaseModelAction: require('./dist/data/basemodel/action'),
   BaseModelStore: require('./dist/data/basemodel/store'),
