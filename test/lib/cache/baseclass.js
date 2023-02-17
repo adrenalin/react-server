517498041a59069cd548e6b280114ffee35a5cfd
Arttu Manninen <arttu@kaktus.cc>
2023-02-17 11:24:33 +0200

    Helper method to handle different expiration formats

--- test/lib/cache/baseclass.js
+++ test/lib/cache/baseclass.js
@@ -1,3 +1,4 @@
+const Moment = require('moment')
 const errors = require('@vapaaradikaali/errors')
 const { expect } = require('chai')
 const CacheBaseclass = require('../../../lib/cache')
@@ -8,7 +9,7 @@ describe('lib/cache', () => {
     config: new Config()
   }
 
-  it('should initialize config', (done) => {
+  it('should initialize config', () => {
     const cacheKey = 'test.key'
     const cacheValue = 'test-value'
     app.config.set(`cache.${cacheKey}`, cacheValue)
@@ -16,11 +17,9 @@ describe('lib/cache', () => {
     const cache = new CacheBaseclass(app)
     expect(cache).to.have.property('config')
     expect(cache.config.get(cacheKey)).to.equal(cacheValue)
-
-    done()
   })
 
-  it('should have methods setStorageKey and getStorageKey', (done) => {
+  it('should have methods setStorageKey and getStorageKey', () => {
     const cache = new CacheBaseclass(app)
     const storageKey = 'storage-key'
 
@@ -29,89 +28,58 @@ describe('lib/cache', () => {
 
     expect(cache.setStorageKey(storageKey)).to.be.an.instanceof(CacheBaseclass)
     expect(cache.getStorageKey()).to.equal(storageKey)
-
-    done()
   })
 
-  it('should have the abstract method "get"', (done) => {
-    const cache = new CacheBaseclass(app)
-    expect(cache.get).to.be.a('function')
-
-    cache.get('foo')
-      .then(() => {
-        done(new Error('Should have thrown an exception'))
-      })
-      .catch((err) => {
-        expect(err).to.be.an.instanceof(errors.NotImplemented)
-        done()
-      })
-  })
-
-  it('should have the abstract method "set"', (done) => {
-    const cache = new CacheBaseclass(app)
-    expect(cache.set).to.be.a('function')
-
-    cache.set('foo', 'bar')
-      .then(() => {
-        done(new Error('Should have thrown an exception'))
-      })
-      .catch((err) => {
-        expect(err).to.be.an.instanceof(errors.NotImplemented)
-        done()
-      })
-  })
-
-  it('should have the abstract method "del"', (done) => {
-    const cache = new CacheBaseclass(app)
-    expect(cache.del).to.be.a('function')
-
-    cache.del('foo')
-      .then(() => {
-        done(new Error('Should have thrown an exception'))
-      })
-      .catch((err) => {
-        expect(err).to.be.an.instanceof(errors.NotImplemented)
-        done()
-      })
-  })
-
-  it('should have the abstract method "expire"', (done) => {
-    const cache = new CacheBaseclass(app)
-    expect(cache.expire).to.be.a('function')
-
-    cache.expire('foo', 1)
-      .then(() => {
-        done(new Error('Should have thrown an exception'))
-      })
-      .catch((err) => {
-        expect(err).to.be.an.instanceof(errors.NotImplemented)
-        done()
-      })
-  })
-
-  it('should throw an error for an invalid cache engine name', (done) => {
+  it('should have the abstract method "get"', async () => {
     try {
-      CacheBaseclass.getEngine(app, 'foo bar')
-      throw new Error('Should have thrown an exception')
-    } catch (err) {
-      expect(err).to.be.an.instanceof(errors.BadRequest)
-      done()
-    }
-  })
-
-  it('should throw an error for a not implemented for unknown cache engine', (done) => {
-    try {
-      CacheBaseclass.getEngine(app, 'foobar')
-      throw new Error('Should have thrown an exception')
+      const cache = new CacheBaseclass(app)
+      expect(cache.get).to.be.a('function')
+      await cache.get('foo')
+      throw new Error('Should have thrown a NotImplemented')
     } catch (err) {
       expect(err).to.be.an.instanceof(errors.NotImplemented)
-      done()
     }
   })
 
-  it('should throw a NotImplemented error for an inexistant cache engine name', (done) => {
+  it('should have the abstract method "set"', async () => {
+    try {
+      const cache = new CacheBaseclass(app)
+      expect(cache.set).to.be.a('function')
+      await cache.set('foo', 'bar')
+      throw new Error('Should have thrown a NotImplemented')
+    } catch (err) {
+      expect(err).to.be.an.instanceof(errors.NotImplemented)
+    }
+  })
+
+  it('should have the abstract method "del"', async () => {
+    try {
+      const cache = new CacheBaseclass(app)
+      expect(cache.del).to.be.a('function')
+      await cache.del('foo')
+      throw new Error('Should have thrown a NotImplemented')
+    } catch (err) {
+      expect(err).to.be.an.instanceof(errors.NotImplemented)
+    }
+  })
+
+  it('should have the abstract method "expire"', async () => {
+    try {
+      const cache = new CacheBaseclass(app)
+      expect(cache.expire).to.be.a('function')
+      await cache.expire('foo', 1)
+      throw new Error('Should have thrown a NotImplemented')
+    } catch (err) {
+      expect(err).to.be.an.instanceof(errors.NotImplemented)
+    }
+  })
+
+  it('should throw an error for an invalid cache engine name', () => {
+    expect(() => CacheBaseclass.getEngine(app, 'foo bar')).to.throw(errors.BadRequest)
+  })
+
+  it('should throw a NotImplemented for a not implemented for unknown cache engine', () => {
     expect(() => CacheBaseclass.getEngine(app, 'foobar')).to.throw(errors.NotImplemented)
-    done()
   })
 
   it('should throw a NotImplemented for cache flush', async () => {
@@ -122,5 +90,49 @@ describe('lib/cache', () => {
     } catch (err) {
       expect(err).to.be.an.instanceof(errors.NotImplemented)
     }
+  })
+
+  it('should return null when given null in getExpiresIn', () => {
+    expect(CacheBaseclass.getExpiresIn(null)).to.equal(null)
+  })
+
+  it('should accept a time as an ISO 8601 period for getExpiresIn', () => {
+    expect(CacheBaseclass.getExpiresIn('P1D')).to.equal(24 * 3600)
+    expect(CacheBaseclass.getExpiresIn('PT1M')).to.equal(60)
+    expect(CacheBaseclass.getExpiresIn('PT5S')).to.equal(5)
+  })
+
+  it('should accept a time as an ISO 8601 timestamp for getExpiresIn', () => {
+    const m = new Moment()
+    m.add(10, 'seconds')
+    expect(CacheBaseclass.getExpiresIn(m.toISOString())).to.eql(10)
+  })
+
+  it('should return zero second expiration for a Moment in the past', () => {
+    const m = new Moment()
+    m.subtract(10, 'seconds')
+    expect(CacheBaseclass.getExpiresIn(m.toISOString())).to.eql(0)
+  })
+
+  it('should reject an invalid string for getExpiresIn', () => {
+    expect(() => CacheBaseclass.getExpiresIn('foo')).to.throw(errors.InvalidArgument)
+  })
+
+  it('should accept a time as a Date for getExpiresIn', () => {
+    const d = new Date(Date.now() + 10 * 1000)
+    expect(CacheBaseclass.getExpiresIn(d)).to.equal(10)
+  })
+
+  it('should accept a time as a Moment for getExpiresIn', () => {
+    const m = new Moment()
+    m.add(10, 'seconds')
+    expect(CacheBaseclass.getExpiresIn(m)).to.eql(10)
+  })
+
+  it('should accept a time as a number for getExpiresIn', () => {
+    expect(CacheBaseclass.getExpiresIn(10)).to.eql(10)
+    expect(CacheBaseclass.getExpiresIn(-10)).to.eql(0)
+    expect(CacheBaseclass.getExpiresIn(0.001)).to.eql(0.001)
+    expect(CacheBaseclass.getExpiresIn(0.0001)).to.eql(0, 'Normalize to milliseconds')
   })
 })
